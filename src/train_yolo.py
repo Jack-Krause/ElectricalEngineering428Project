1f995bc4c2fab3791899d14858113341afbfd3ac
Ubuntu <ubuntu@ip-172-31-67-185.ec2.internal>
2025-12-31 20:52:12 +0000

    refactored for dynamic selection of models

--- src/train_yolo.py
+++ src/train_yolo.py
@@ -4,7 +4,8 @@ from ultralytics import YOLO
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 
-def train_baseline(
+def train(
+    model_weights: str = "yolov8n.pt",
     data_config: str = "configs/soda.yaml",
     epochs: int = 10,
     imgsz: int = 640,
@@ -29,5 +30,34 @@ def train_baseline(
     )
 
 
+'''
+YOLOv8m compared to YOLOv8n:
+v8n (nano): 3.2M params
+v8m: 25.9M parameters
+'''
+def train_yolov8m(
+    data_config: str = "configs/soda.yaml",
+    epochs: int = 10,
+    imgsz: int = 640,
+    batch: int = 8,
+):
+    
+    model = YOLO("yolov8m.pt") ## NOTE: need to ?download this?
+
+    model.train(
+        data=str(PROJECT_ROOT / data_config),
+        epochs=epochs,
+        imgsz=imgsz,
+        batch=batch,
+        workers=2,
+        device=0,
+        # modifed for test 3
+        multi_scale=True,
+        scale=0.9,
+        hsv_h=0.02,
+        hsv_s=0.7,
+        hsv_v=0.4
+    )
+
 if __name__ == "__main__":
-    train_baseline()
+    train()
